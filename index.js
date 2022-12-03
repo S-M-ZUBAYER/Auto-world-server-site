d04d98807914dae580d87350964c82583762a840
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-12-03 09:28:31 +0600

    product post and get  server set up completed

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-// const express = require('express');
-// const cors = require('cors');
-// const { MongoClient, ObjectId } = require('mongodb');
-// require('dotenv').config();
-// const port = process.env.PORT || 5000;
-
-// //USER_DB=auto_worldDB
-// //PASSWORD=Badw9qq27RacInXt
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p2sr91x.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// async function run() {
-//     try {
-
-//         const usersCollections = client.db('Cricket_Lover').collection('users');
-
-//         console.log('database connected ')
-//     }
-
-//     finally {
-
-//     }
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 
-// }
+const app = express();
 
-// run().catch(console.log)
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p2sr91x.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+
+        const productsCollections = client.db('auto_world').collection('products');
+        console.log('database connected ');
+
+        app.post('/products', async (req, res) => {
+            const user = req.body;
+            const result = await productsCollections.insertOne(user);
+            res.send(result);
+        });
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const users = await productsCollections.find(query).toArray();
+            res.send(users)
+        })
+    }
+
+    finally {
+
+    }
 
 
-// app.get('/', async (req, res) => {
-//     res.send('Cricket lover server is running');
-// });
+}
 
-// app.listen(port, () => console.log(`Cricket lover is running on ${port}`))
+run().catch(console.log)
+
+
+app.get('/', async (req, res) => {
+    res.send('Cricket lover server is running');
+});
+
+app.listen(port, () => console.log(`Cricket lover is running on ${port}`))
